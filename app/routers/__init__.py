57e8abe32259f7bf2886e462c8d3da9ffe426747
rinward23 <crgame15@gmail.com>
2025-09-27 13:31:20 -0400

    Add synastry and composite API endpoints

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,6 +3,12 @@
 
 from .aspects import router as aspects_router
 from .policies import router as policies_router
+from .rel import router as rel_router
 from .transits import router as transits_router
 
-__all__ = ["aspects_router", "policies_router", "transits_router"]
+__all__ = [
+    "aspects_router",
+    "policies_router",
+    "rel_router",
+    "transits_router",
+]
---
+++ core/__init__.py
@@ -0,0 +1,3 @@
+"""AstroEngine Plus compatibility shims for lightweight API services."""
+
+__all__ = ["rel_plus"]
