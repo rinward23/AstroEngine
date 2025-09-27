26576aa32a2f664cdf7a114467b5076300f8a54e
rinward23 <crgame15@gmail.com>
2025-09-27 15:26:19 -0400

    Add relationship interpretation scaffolding (#182)

---
+++ core/interpret_plus/__init__.py
@@ -0,0 +1,11 @@
+"""Relationship interpretation scaffolding utilities."""
+
+from .engine import ASPECT_SYMBOLS, ARCHETYPES, Finding, interpret, load_rules
+
+__all__ = [
+    "ASPECT_SYMBOLS",
+    "ARCHETYPES",
+    "Finding",
+    "interpret",
+    "load_rules",
+]
