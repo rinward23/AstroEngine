d3f999779b24afd39810aa5c41330c78c017f9e0
rinward23 <crgame15@gmail.com>
2025-09-27 20:56:22 -0400

    feat(synastry): add midpoint scanner hotspots (#195)

--- astroengine/synastry/__init__.py
+++ astroengine/synastry/__init__.py
@@ -2,51 +2,25 @@
 
 from __future__ import annotations
 
-from .engine import (
-    ASPECT_FAMILY_MAP,
-    CHALLENGING_ASPECTS,
-    ChartPositions,
-    DEFAULT_ASPECT_SET,
-    DEFAULT_ORB_POLICY,
-    DEFAULT_WEIGHTS,
-    EclipticPosition,
-    GridCell,
-    HARMONIOUS_ASPECTS,
-    Hit,
-    NEUTRAL_ASPECTS,
-    OrbPolicy,
-    Overlay,
-    OverlayLine,
-    Scores,
-    Weights,
-    build_grid,
-    compute_scores,
-    detect_hits,
-    make_overlay,
+
+from .midpoints import (
+    MidpointHit,
+    MidpointHotspot,
+    MidpointScanResult,
+    OverlayMarker,
+    scan_midpoints,
+
 )
 from .orchestrator import SynHit, compute_synastry
 
 __all__ = [
     "SynHit",
     "compute_synastry",
-    "ChartPositions",
-    "EclipticPosition",
-    "Hit",
-    "GridCell",
-    "Overlay",
-    "OverlayLine",
-    "Scores",
-    "OrbPolicy",
-    "Weights",
-    "DEFAULT_ORB_POLICY",
-    "DEFAULT_WEIGHTS",
-    "DEFAULT_ASPECT_SET",
-    "HARMONIOUS_ASPECTS",
-    "CHALLENGING_ASPECTS",
-    "NEUTRAL_ASPECTS",
-    "ASPECT_FAMILY_MAP",
-    "detect_hits",
-    "build_grid",
-    "make_overlay",
-    "compute_scores",
+
+    "MidpointHit",
+    "MidpointHotspot",
+    "OverlayMarker",
+    "MidpointScanResult",
+    "scan_midpoints",
+
 ]
