22a7bd4aa62779903407a030f0974b2480cf2025
rinward23 <crgame15@gmail.com>
2025-10-05 19:21:21 -0400

    Add chart library persistence and UI

--- astroengine/analysis/__init__.py
+++ astroengine/analysis/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from .dignities import condition_report, score_accidental, score_essential
 from .midpoints import compute_midpoints, get_midpoint_settings, midpoint_longitude
+from .declinations import DeclinationAspect, declination_aspects, get_declinations
 from .returns import aries_ingress_year, lunar_return_datetimes, solar_return_datetime
 from .timeline import (
     VoidOfCourseEvent,
@@ -17,6 +18,9 @@ __all__ = [
     "compute_midpoints",
     "get_midpoint_settings",
     "midpoint_longitude",
+    "DeclinationAspect",
+    "declination_aspects",
+    "get_declinations",
     "condition_report",
     "score_accidental",
     "score_essential",
